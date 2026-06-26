fe6f9fe61479cc97c37a9f06d0bb1222ef83847f
PIETRO RAEL FREDDI DUARTE <05260883039@senacrs.edu.br>
2026-06-25 21:28:56 -0300

    commit

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    mensagem = "<h1>bem-vindo ao DevBlog</h1> <p>Em breve arquivos aqui.</p>"
+
+    return HttpResponse(mensagem)
+
+def sobre(request):
+    mensagem = "<h1><strong>Sobre o DevBlog</strong></h1> <p><em>testes <br>gostosos</em> 😍</p> "
+
+    return HttpResponse(mensagem)
