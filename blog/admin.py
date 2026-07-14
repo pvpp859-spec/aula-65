9e33a97cab4514d79fdb9be45fac55a2679aead6
pvpp859-spec <pvpp859@gmail.com>
2026-07-14 20:57:37 -0300

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -12,3 +12,12 @@ class ArtigoAdmin(admin.ModelAdmin):
     search_fields = ('titulo', 'conteudo')
     
     list_filter = ('Categoria', 'data_publicacao')
+
+from django.contrib.auth.models import User
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
+
+@receiver(post_migrate)
+def create_superuser(sender, **kwargs):
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@admin.com', 'senha123')
